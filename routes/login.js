d24a547a7a01d66bae0b52f6d8df89fb094a0b42
somaranaresh <naresh.snc@gmail.com>
2018-03-25 00:09:45 +0530

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -4,38 +4,26 @@
 var express = require('express');
 var router = express.Router();
 router.route('/').post(function(req, res, next){
-    try{
-        var isInvalid = bookFinderLib.mandatoryParametersMissing(req.body, ['username','password']);
-        if(isInvalid){
-            logger.info(`At User : Save - required parameter missing ${isInvalid}`);
-            res.render('login', {
-                message : 'Mandatory parameter missing'
-            });
-        }
-        mongo.Users.findOne({username : req.body.username}, {}, function (mongoErr, mongoRes) {
-            try{
-                logger.info(`At Login : Users check user is valid err : ${mongoErr}, res : ${util.format('%j', mongoRes)}`);
-                if(mongoRes){
-                    loginUserData[mongoRes.username] = {name : mongoRes.name, role : mongoRes.role};
-                    return res.redirect('/books');
-                }
-                return res.render('login', {
-                    message : 'Please enter valid username'
-                });
-            }catch (err){
-                logger.error(err.stack);
-                return res.render('login', {
-                    message : 'Internal server error'
-                });
-            }
-        });
-
-    }catch (err){
-        logger.error(err.stack);
-        return res.render('login', {
-            message : 'Internal server error'
+    var isInvalid = bookFinderLib.mandatoryParametersMissing(req.body, ['username','password']);
+    if(isInvalid){
+        logger.info(`At User : Save - required parameter missing ${isInvalid}`);
+        res.render('login', {
+            message : 'Mandatory parameter missing'
         });
     }
+    mongo.Users.findOne({username : req.body.username}, {}, function (mongoErr, mongoRes) {
+        logger.info(`At Login : Users check user is valid err : ${mongoErr}, res : ${util.format('%j', mongoRes)}`);
+        if(mongoRes){
+            req.session.name =  mongoRes.name;
+            req.session.role =  mongoRes.role;
+            return res.redirect('/books');
+        }
+        return res.render('login', {
+            message : 'Please enter valid username'
+        });
+    });
+}).get(function (req, res, next) {
+    return res.render('login', { message : ''  });
 })
 
 module.exports = router;
